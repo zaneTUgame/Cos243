a47036a5960f363d6de298c068c6c96ab6f57e5e
GitHub Username here <email shared with GitHub here>
2024-02-14 10:44:38 -0500

    Added sample HTML file that contain basic syntax

--- Ent.js
+++ Ent.js
@@ -10,7 +10,15 @@ function Dis1ChiW(){
 function Dis2ChiW(){
     document.getElementsByClassName("hide")[1].style.display = "none";
 }
+function Dis1Chi(){
+    document.getElementsByClassName("hide")[2].style.display = "";
+}
+function Dis2Chi(){
+    document.getElementsByClassName("hide")[2].style.display = "none";
+}
 ChickenPP.addEventListener("mouseover", Dis1ChiPP);
 chpptext.addEventListener("mouseout", Dis2ChiPP);
 ChickenW.addEventListener("mouseover", Dis1ChiW);
 chWtext.addEventListener("mouseout", Dis2ChiW);
+Chicken.addEventListener("mouseover", Dis1Chi);
+chtext.addEventListener("mouseout", Dis2Chi);
